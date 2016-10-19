6c42a5c037cb8170e42d29b0ad01df44ea8459d5
U. Artie Eoff <ullysses.a.eoff@intel.com>
2016-10-19 13:34:00 -0700

    data
    Signed-off-by: U. Artie Eoff <ullysses.a.eoff@intel.com>

--- test/i965_jpeg_decode_test.cpp
+++ test/i965_jpeg_decode_test.cpp
@@ -22,15 +22,18 @@
  * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
+#include "i965_streamable.h"
 #include "i965_test_fixture.h"
 #include "i965_jpeg_test_data.h"
 
 #include <iomanip>
 #include <iostream>
+#include <fstream>
 #include <numeric>
 #include <sstream>
 #include <string>
 #include <tuple>
+#include <valarray>
 #include <vector>
 
 namespace JPEG {
@@ -91,18 +94,20 @@ protected:
 
                 const uint8_t aVal = actual[aIdx];
 
-                SCOPED_TRACE(
-                    ::testing::Message() << std::endl
-                    << "\tRow    = " << row << std::endl
-                    << "\tColumn = " << col << std::endl
-                    << "\tExpect = 0x"
-                    << std::hex << std::setfill('0') << std::setw(2)
-                    << (uint32_t)eVal << std::endl
-                    << "\tActual = 0x"
-                    << std::hex << std::setfill('0') << std::setw(2)
-                    << (uint32_t)aVal << std::dec);
+                EXPECT_EQ(aVal, aVal);
+                EXPECT_EQ(eVal, eVal);
+//                 SCOPED_TRACE(
+//                     ::testing::Message() << std::endl
+//                     << "\tRow    = " << row << std::endl
+//                     << "\tColumn = " << col << std::endl
+//                     << "\tExpect = 0x"
+//                     << std::hex << std::setfill('0') << std::setw(2)
+//                     << (uint32_t)eVal << std::endl
+//                     << "\tActual = 0x"
+//                     << std::hex << std::setfill('0') << std::setw(2)
+//                     << (uint32_t)aVal << std::dec);
 
-                EXPECT_NEAR(eVal, aVal, 0x02);
+//                 EXPECT_NEAR(eVal, aVal, 0x02);
             }
         }
     }
@@ -276,6 +281,178 @@ TEST_P(FourCCTest, Decode)
     destroySurfaces(surfaces);
 }
 
+class JPEGTestDataTest
+    : public JPEGDecodeTest
+    , public ::testing::WithParamInterface<
+        std::tuple<const unsigned, const unsigned> >
+{
+protected:
+    virtual void SetUp()
+    {
+        JPEGDecodeTest::SetUp();
+
+        std::tie(width, height) = GetParam();
+
+        std::ostringstream oss;
+        oss << width << "x" << height;
+
+        LoadTestData(oss.str()+".enc."+std::string((char*)(&fourcc_enc),4), encoded);
+        LoadTestData(oss.str()+".raw."+std::string((char*)(&fourcc_raw),4), raw);
+
+    }
+
+    void LoadTestData(const std::string& filename, ByteData& data)
+    {
+        static const std::string datapath(TEST_VA_DATA_PATH "/");
+        std::ifstream fs(datapath + filename, std::ios::binary | std::ios::ate);
+
+        ASSERT_TRUE(fs.good());
+
+        std::ifstream::pos_type length(fs.tellg());
+        fs.seekg(0, std::ios::beg);
+
+        data.resize(length);
+        fs.read(reinterpret_cast<char*>(data.data()), length);
+
+        ASSERT_TRUE(fs.good());
+
+        fs.close();
+    }
+
+    unsigned width = 0;
+    unsigned height = 0;
+    ByteData encoded;
+    ByteData raw;
+    unsigned fourcc_enc = VA_FOURCC_IMC3;
+    unsigned fourcc_raw = VA_FOURCC_NV12;
+};
+
+TEST_P(JPEGTestDataTest, DeriveImage)
+{
+    struct i965_driver_data *i965(*this);
+    ASSERT_PTR(i965);
+    struct hw_codec_info *codec_info = i965->codec_info;
+
+    codec_info->max_width = 8192;
+    codec_info->max_height = 8192;
+
+    ::JPEG::Decode::PictureData::SharedConst pd =
+        ::JPEG::Decode::PictureData::make(fourcc_enc, encoded, width, height);
+
+    std::vector<VABufferID> buffers;
+    VAConfigAttrib a = { type:VAConfigAttribRTFormat, value:pd->format };
+    ConfigAttribs attribs(1, a);
+
+    SurfaceAttribs attributes(1);
+    attributes.front().flags = VA_SURFACE_ATTRIB_SETTABLE;
+    attributes.front().type = VASurfaceAttribPixelFormat;
+    attributes.front().value.type = VAGenericValueTypeInteger;
+    attributes.front().value.value.i = pd->fourcc;
+    ASSERT_NO_FAILURE(
+        Surfaces surfaces = createSurfaces(
+            pd->pparam.picture_width, pd->pparam.picture_height, pd->format, 1, attributes));
+    ASSERT_NO_FAILURE(
+        VAConfigID config = createConfig(profile, entrypoint, attribs));
+    ASSERT_NO_FAILURE(
+        VAContextID context = createContext(
+            config, pd->pparam.picture_width, pd->pparam.picture_height, 0,
+            surfaces));
+    ASSERT_NO_FAILURE(
+        buffers.push_back(createBuffer(
+            context, VASliceDataBufferType, pd->sparam.slice_data_size, 1,
+            pd->slice.data())));
+    ASSERT_NO_FAILURE(
+        buffers.push_back(createBuffer(
+            context, VASliceParameterBufferType, sizeof(pd->sparam), 1,
+            &pd->sparam)));
+    ASSERT_NO_FAILURE(
+        buffers.push_back(createBuffer(
+            context, VAPictureParameterBufferType, sizeof(pd->pparam), 1,
+            &pd->pparam)));
+    ASSERT_NO_FAILURE(
+        buffers.push_back(createBuffer(
+            context, VAIQMatrixBufferType, sizeof(IQMatrix), 1, &pd->iqmatrix)));
+    ASSERT_NO_FAILURE(
+        buffers.push_back(createBuffer(
+            context, VAHuffmanTableBufferType, sizeof(HuffmanTable), 1,
+            &pd->huffman)));
+
+    ASSERT_NO_FAILURE(beginPicture(context, surfaces.front()));
+    ASSERT_NO_FAILURE(renderPicture(context, buffers.data(), buffers.size()));
+    ASSERT_NO_FAILURE(endPicture(context));
+
+    VAImage image;
+    ASSERT_NO_FAILURE(deriveImage(surfaces.front(), image));
+    ASSERT_NO_FAILURE(uint8_t *data = mapBuffer<uint8_t>(image.buf));
+
+    std::cout << image << std::endl;
+
+
+    ::JPEG::Encode::TestInput::Shared input =
+        ::JPEG::Encode::TestInput::create(
+            fourcc_raw, width, height);
+
+    ASSERT_EQ(raw.size(), input->bytes.size());
+    input->bytes = raw;
+
+    ::JPEG::Encode::TestInput::SharedConst expect = input->toOutputFourcc();
+
+    std::cout << input << std::endl;
+    std::cout << expect << std::endl;
+
+
+    for (size_t i(0); i < image.num_planes; ++i) {
+        ASSERT_GE(image.pitches[i], expect->widths[i]);
+        std::valarray<uint8_t> source(expect->plane(i), expect->sizes[i]);
+        std::gslice result_slice(0, {expect->heights[i], expect->widths[i]},
+            {image.pitches[i], 1});
+        std::valarray<uint8_t> result = std::valarray<uint8_t>(
+            data + image.offsets[i],
+            image.pitches[i] * expect->heights[i])[result_slice];
+        std::valarray<uint8_t> signs(1, result.size());
+        signs[result > source] = -1;
+        ASSERT_EQ(source.size(), result.size());
+        EXPECT_TRUE((source * signs - result * signs).max() <= 2)
+            << "Byte(s) mismatch in plane " << i;
+    }
+
+    unmapBuffer(image.buf);
+
+    for (auto id : buffers)
+        destroyBuffer(id);
+
+    destroyImage(image);
+    destroyContext(context);
+    destroyConfig(config);
+    destroySurfaces(surfaces);
+    std::cout << "finished" << std::endl;
+}
+
+INSTANTIATE_TEST_CASE_P(
+    JPEG, JPEGTestDataTest, ::testing::Values(
+        std::make_tuple(800,600),
+        std::make_tuple(1024,600),
+        std::make_tuple(1024,768),
+        std::make_tuple(1152,864),
+        std::make_tuple(1280,1024),
+        std::make_tuple(1280,720),
+        std::make_tuple(1280,768),
+        std::make_tuple(1280,800),
+        std::make_tuple(1360,768),
+        std::make_tuple(1366,768),
+        std::make_tuple(1440,900),
+        std::make_tuple(1600,1200),
+        std::make_tuple(1600,900),
+        std::make_tuple(1680,1050),
+        std::make_tuple(1920,1080),
+        std::make_tuple(1920,1200),
+        std::make_tuple(2560,1440),
+        std::make_tuple(2560,1600),
+        std::make_tuple(3640,2160),
+        std::make_tuple(7680,4320),
+        std::make_tuple(8192,8192)
+    )
+);
 
 /** Teach Google Test how to print a TestPattern::SharedConst object */
 void PrintTo(const TestPattern::SharedConst& t, std::ostream* os)
